88e723f9c7c91adafd5399c65d8c6a979d4cbf65
Pratyasa Roy <proy8892@conestogac.on.ca>
2026-06-20 19:22:24 -0400

    Added user and queue structures

--- queueAssignment/queueAssignment/queue.h
+++ queueAssignment/queueAssignment/queue.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define USERNAME_SIZE 11
+
+typedef struct {
+    char username[USERNAME_SIZE];
+    int level;
+    char faction[10];
+} User;
+
+typedef struct QueueNode {
+    User user;
+    struct QueueNode* next;
+} QueueNode;
+
+typedef struct {
+    QueueNode* head;
+    QueueNode* tail;
+} Queue;
+
+#endif
