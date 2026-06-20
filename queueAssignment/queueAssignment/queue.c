c6b123029420a71344a2be25a1bb39761e0f5a43
Pratyasa Roy <proy8892@conestogac.on.ca>
2026-06-20 19:34:40 -0400

    Added initializeQueue function

--- queueAssignment/queueAssignment/queue.c
+++ queueAssignment/queueAssignment/queue.c
@@ -1,1 +1,8 @@
 #include "queue.h"
+#include <stdlib.h>
+
+void initializeQueue(Queue* queue)// Function to initialize the queue by setting head and tail to NULL
+{
+	queue->head = NULL;// Set the head of the queue to NULL
+	queue->tail = NULL;// Set the tail of the queue to NULL
+}
--- queueAssignment/queueAssignment/queue.h
+++ queueAssignment/queueAssignment/queue.h
@@ -1,22 +1,26 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#define USERNAME_SIZE 11
+#define USERNAME_SIZE 11// 10 characters + null terminator
 
-typedef struct {
-    char username[USERNAME_SIZE];
-    int level;
-    char faction[10];
-} User;
+typedef struct//User structure to hold user information 
+{
+	char username[USERNAME_SIZE];// Username of the user
+	int level;// Level of the user
+	char faction[10];// Faction of the user
+} User;//User structure to hold user information
 
-typedef struct QueueNode {
-    User user;
-    struct QueueNode* next;
-} QueueNode;
+typedef struct QueueNode// Node structure for the queue, containing a User and a pointer to the next node
+{
+	User user;// User information stored in the node
+	struct QueueNode* next;// Pointer to the next node in the queue
+} QueueNode;// Node structure for the queue, containing a User and a pointer to the next node
 
-typedef struct {
-    QueueNode* head;
-    QueueNode* tail;
+typedef struct // Queue structure containing pointers to the head and tail of the queue
+{
+	QueueNode* head;// Pointer to the head of the queue
+	QueueNode* tail;// Pointer to the tail of the queue
 } Queue;
+void initializeQueue(Queue* queue);// Function prototype for initializing the queue
 
 #endif
